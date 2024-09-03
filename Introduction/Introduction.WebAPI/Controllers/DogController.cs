9e3236adcdc22bcfc6854da1c7e23538631aaaba
Deni253 <73397544+Deni253@users.noreply.github.com>
2024-09-03 15:56:39 +0200

    Add files via upload
    Uvod u API, CRUD, REQUEST-RESPONSE,CONTROLLER

---
+++ Introduction/Introduction.WebAPI/Controllers/DogController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DogController : ControllerBase
+    {
+
+        static List<Dog> dogs = new List<Dog>();    
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult PostNewDog(Dog dog)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            {
+                dogs.Add(dog);
+            }
+            return Ok(dogs);
+        }
+
+        [HttpDelete]
+        [Route("del/{id}")]
+        public IActionResult DeleteDog(int id)
+        {
+            if (!ModelState.IsValid || id < 0)
+                return BadRequest("Invalid data.");
+            {
+                foreach (Dog dog in dogs.ToList()) if (dog.Id == id)
+                    {
+                        dogs.Remove(dog);                          
+                    }
+            }
+            return Ok(dogs);
+        }
+
+        [HttpDelete]
+        [Route("deleteall")]
+        public IActionResult DeleteDogs()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            {
+                foreach (Dog dog in dogs.ToList())
+                    {
+                        dogs.Remove(dog);
+                    }
+            }
+            return Ok(dogs);
+        }
+
+        [HttpGet]
+        [Route("get/all")]
+
+        public IEnumerable<Dog> GetDogs()
+        {
+            return dogs;
+        }
+
+        [HttpPut]
+        [Route("update/train/{id}")]
+        public IActionResult UpdateDog(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            {
+                foreach(Dog dog in dogs) if (dog.Id == id)
+                {
+                        dog.IsTrained=true;
+                }   
+            }
+            return Ok(dogs);
+        }
+    }
+}
---
+++ Introduction/Introduction.WebAPI/Dog.cs
@@ -0,0 +1,25 @@
+﻿namespace Introduction.WebAPI
+{
+    public class Dog
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age { get; set; }
+        public string? FurColor { get; set; }
+        public bool? IsTrained { get; set; }
+
+
+
+        public Dog() { }
+
+        public Dog(string name, DateTime birthDate, int age, string furColor)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Age = age;
+            FurColor = furColor;
+        }
+    }
+}
